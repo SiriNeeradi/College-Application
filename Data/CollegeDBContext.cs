2ba891b17196d9324bcae03288d863d5883b2876
SiriNeeradi <sirineeradi07@gmail.com>
2026-06-09 15:43:56 +0530

    Moving Entity Type Configurations to seperate File

--- Data/CollegeDBContext.cs
+++ Data/CollegeDBContext.cs
@@ -15,33 +15,8 @@ namespace CollegeApp.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>().HasData(new List<Student>()
-            {
-                new Student
-                {
-                    Id          = 1,
-                    StudentName = "Alice",
-                    Address     = "India",
-                    Email       = "alice@gmail.com",
-                    DOB         = new DateTime(2022, 12, 12)
-                },
-                new Student
-                {
-                    Id          = 2,
-                    StudentName = "Tom",
-                    Address     = "India",
-                    Email       = "tom@gmail.com",
-                    DOB         = new DateTime(2022, 10, 12)
-                }
-
-            });
-
-            modelBuilder.Entity<Student>(entity =>
-            {
-                entity.Property(n => n.StudentName).IsRequired().HasMaxLength(50);
-                entity.Property(n => n.Address).IsRequired(false).HasMaxLength(200);
-                entity.Property(n => n.Email).IsRequired().HasMaxLength(200);
-            });
+            //Table1
+            modelBuilder.ApplyConfiguration(new Config.StudentConfig());
         }
 
     }
--- Data/Config/StudentConfig.cs
+++ Data/Config/StudentConfig.cs
@@ -7,7 +7,36 @@ namespace CollegeApp.Data.Config
     {
         public void Configure(EntityTypeBuilder<Student> builder)
         {
-            //builder.ToTable("Students");
+            builder.ToTable("Students");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+
+            builder.Property(n => n.StudentName).IsRequired().HasMaxLength(50);
+            builder.Property(n => n.Address).IsRequired(false).HasMaxLength(200);
+            builder.Property(n => n.Email).IsRequired().HasMaxLength(200);
+
+            builder.HasData(new List<Student>()
+            {
+                new Student
+                {
+                    Id          = 1,
+                    StudentName = "Alice",
+                    Address     = "India",
+                    Email       = "alice@gmail.com",
+                    DOB         = new DateTime(2022, 12, 12)
+                },
+                new Student
+                {
+                    Id          = 2,
+                    StudentName = "Tom",
+                    Address     = "India",
+                    Email       = "tom@gmail.com",
+                    DOB         = new DateTime(2022, 10, 12)
+                }
+
+            });
         }
     }
 }
+
